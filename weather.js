65497e50563b5034e0c7c34cac3c3f8458e863d9
Mi Mapuwa <mapuwa@protonmail.com>
2016-07-16 19:44:31 +0200

    get location and address

--- weather.js
+++ weather.js
@@ -1,10 +1,44 @@
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            document.getElementById("demo").innerHTML = xhttp.responseText;
-        }
-    };
-    xhttp.open("GET", "ajax_info.txt", true);
-    xhttp.send();
+function findLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+                address: ""
+            };
+            var xhttp = new XMLHttpRequest();
+            xhttp.open("GET", "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + pos.lat + "," + pos.lng, false);
+            xhttp.send();
+            var response = JSON.parse(xhttp.responseText);
+
+            if (response.status == "OK") {
+                pos.address = response.results[0].formatted_address;
+            }
+            showWeather(pos);
+        }, function() {
+            findByIP();
+        });
+    } else {
+        findByIP();
+    }
 }
+
+function  findByIP() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://ip-api.com/json", false);
+    xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
+    var pos = {
+        lat: response.lat,
+        lng: response.lon,
+        address: response.city + ", " + response.country
+    };
+    showWeather(pos);
+}
+
+function showWeather(position) {
+    console.log(position);
+}
+
+findLocation();
+findByIP();
